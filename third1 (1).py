9c3d2c3aa4f904da30aba007a49ba01f0da62a72
sontamreddy <92925391+sontamreddy@users.noreply.github.com>
2021-10-29 19:28:27 +0530

    Add files via upload

---
+++ third1 (1).py
@@ -0,0 +1,29 @@
+"""
+alerts or browser pops are of three types
+1. alerts with message and ok/accept button
+2. alerts with message, ok/accept and dismiss/cancel button
+3. alerts with message, ok/accept, dismiss/cancel and input box 
+ we can handle alerts with selenium using switch_to function and alert from driver class.
+"""
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome(
+    executable_path=r"C:\Users\tejas\Desktop\pytest_sample\Test\chromedriver.exe"
+)
+driver.get(r"file:C:\Users\tejas\Desktop\pytest_sample\Test\class_mods\third1.html")
+time.sleep(3)
+#find_element_by_tag_name("button") will give the button element over which cilck() 
+#function will do the cilck operation.
+driver.find_element_by_tag_name('button').click()
+time.sleep(3)
+#driver.switch_to.alert the alert object by which we get text, or we can accept / dismiss alert.
+#In the below step i have assigned/ store the reference of alert obj in alert_obj
+#returned by driver.switch_to.alert
+alert_obj = driver.switch_to.alert
+
+#.text with return the text of any element here it will return the text of alert.
+print(alert_obj.text)
+time.sleep(5)
+#.accept() will not take any agruments but accepts the alert/ click ok button
+alert_obj.accept()
